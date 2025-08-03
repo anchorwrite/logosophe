bf6d5a7e195b9a92e070c739a4b666adadacbf36
Philip Lowden <phil@dreamtone.com>
2025-08-02 20:00:38 -0400

    turn off R2 incremental caching

--- apps/worker/open-next.config.ts
+++ apps/worker/open-next.config.ts
@@ -1,8 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
  
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
+  incrementalCache: undefined,
   // Disable cache interception to fix static asset serving issues
   enableCacheInterception: false,
 }); 
