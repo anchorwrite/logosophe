dca8e5e565de90988b78611e4ba68a03a2c0aa90
Philip Lowden <phil@dreamtone.com>
2025-08-31 14:08:58 -0400

    fix handle limits 2

--- apps/worker/app/api/dashboard/handle-limits/route.ts
+++ apps/worker/app/api/dashboard/handle-limits/route.ts
@@ -56,7 +56,6 @@ export async function GET(request: NextRequest) {
       SELECT 
         ishl.Id,
         ishl.SubscriberEmail,
-        ishl.MaxHandles,
         ishl.LimitType,
         ishl.Description,
         ishl.SetBy,
