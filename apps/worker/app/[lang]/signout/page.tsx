5cc56a1b42d14a8c0ea0ce3b8b70d40d7f1cfca2
Philip Lowden <phil@dreamtone.com>
2025-09-02 16:54:20 -0400

    internationalize signin and signout

--- apps/worker/app/[lang]/signout/page.tsx
+++ apps/worker/app/[lang]/signout/page.tsx
@@ -1,18 +1,25 @@
 import { Text, Container, Flex, Box, Card } from '@radix-ui/themes'
 import { SignOutButtons } from './sign-out-buttons'
+import { getDictionary } from '@/lib/dictionary'
+import type { Locale } from '@/types/i18n'
 
-
-export default async function SignOutPage() {
+export default async function SignOutPage({
+  params,
+}: {
+  params: Promise<{ lang: string }>
+}) {
+  const { lang } = await params;
+  const dict = await getDictionary(lang as Locale) as any;
   return (
     <Container size="2">
       <Flex direction="column" align="center" justify="start" style={{ minHeight: '100vh', paddingTop: '20vh' }}>
         <Card size="2">
           <Box p="4">
-            <Text as="div" size="6" mb="4" align="center">Sign Out</Text>
+            <Text as="div" size="6" mb="4" align="center">{dict.signout.title}</Text>
             <Text as="p" size="3" mb="4" align="center">
-              Are you sure you want to sign out?
+              {dict.signout.confirmMessage}
             </Text>
-            <SignOutButtons />
+            <SignOutButtons lang={lang} />
           </Box>
         </Card>
       </Flex>
